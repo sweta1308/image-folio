3b8541f167877e1dcb8e93f4797021f6a1868efa
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 21:20:12 +0530

    style: update navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,29 +14,27 @@ export const Navbar = () => {
         <div className="w-full px-[60px] py-[20px] shadow-md flex items-center md:flex-col md:items-start sm:px-[20px]">
           <h1
             onClick={() => navigate("/")}
-            className="text-primary-color font-extrabold text-[36px] cursor-pointer"
+            className="text-primary-color font-extrabold text-[36px] cursor-pointer md:text-[30px] sm:text-[26px]"
           >
             ImageFolio
           </h1>
-          <div className="flex ml-[400px] xl:ml-[200px] md:ml-0 md:mt-[15px]">
-            <div className="border-b-2 border-black p-[5px]">
-              <Search sx={{ fontSize: 15 }} />
-              <input
-                onClick={() => navigate("/")}
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                className="outline-none text-sm mx-[8px] w-[250px] xl:w-[180px] md:w-[250px] sm:w-[150px]"
-                placeholder="Search..."
-              />
-            </div>
+          <div className="rounded-md flex ml-[400px] border border-black xl:ml-[200px] md:ml-0 md:mt-[15px]">
+            <input
+              onClick={() => navigate("/")}
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              className="outline-none text-sm mx-[8px] p-[5px] w-[400px] lg:w-[300px] xs:w-[250px]"
+              placeholder="Search..."
+            />
             <button
+              disabled={searchInput.length === 0}
               onClick={() => {
                 dispatch(searchImage(searchInput));
                 setSearchInput("");
               }}
-              className="ml-[20px] bg-primary-color text-white px-[10px] rounded-md hover:bg-primary-dark"
+              className="bg-primary-color text-white px-[10px] py-[5px] hover:bg-primary-dark"
             >
-              Search
+              <Search sx={{ fontSize: 25 }} />
             </button>
           </div>
         </div>

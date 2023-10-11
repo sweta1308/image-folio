5b3d27da7dcb540cdf85f3e0971f23930051c5a4
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 12:51:16 +0530

    feat: add actions for images

---
+++ src/actions/imageAction.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export const fetchImages = () => async (dispatch) => {
+  dispatch({ type: "LOADING" });
+  try {
+    const { status, data } = await axios.get(
+      "https://api.unsplash.com/photos?page=1&client_id=CyU8YsvA7ySMnH4oaTVYLnNDlIfXer9lFpf-TEhuVEQ"
+    );
+    if (status === 200) {
+      dispatch({ type: "GET_IMAGES", payload: data });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const fetchSingleImage = (id) => async (dispatch) => {
+  dispatch({ type: "LOADING" });
+  try {
+    const { status, data } = await axios.get(
+      `https://api.unsplash.com/photos/${id}?client_id=CyU8YsvA7ySMnH4oaTVYLnNDlIfXer9lFpf-TEhuVEQ`
+    );
+    if (status === 200) {
+      dispatch({ type: "GET_IMAGE", payload: data });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const searchImage = (searchInput) => async (dispatch) => {
+  dispatch({ type: "LOADING" });
+  try {
+    const { status, data } = await axios.get(
+      `https://api.unsplash.com/search/photos?page=1&query=${searchInput}&client_id=CyU8YsvA7ySMnH4oaTVYLnNDlIfXer9lFpf-TEhuVEQ`
+    );
+    if (status === 200) {
+      dispatch({ type: "SEARCH_IMAGE", payload: data.results });
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};

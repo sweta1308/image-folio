32dd28f002a3b5aa44eab4d61309cd7948dbd31f
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 20:57:07 +0530

    refactor: made separate component

--- src/pages/Image.jsx
+++ src/pages/Image.jsx
@@ -1,13 +1,19 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { MoonLoader } from "react-spinners";
-import { handleShare } from "../utils/handleShare";
-import { Download, Share } from "@mui/icons-material";
-import { downloadImage } from "../actions/imageAction";
+import { SingleImage } from "../components/SingleImage";
+import { fetchSingleImage } from "../actions/imageAction";
 
 export const Image = () => {
-  const dispatch = useDispatch();
   const state = useSelector((state) => state);
   const { loading, image } = state;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchSingleImage(id));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
   return (
     <>
       {loading && !image ? (
@@ -15,53 +21,7 @@ export const Image = () => {
           <MoonLoader color="#377dff" />
         </div>
       ) : (
-        <div className="flex justify-center mb-[20px]">
-          <div className="w-[500px] mt-[20px] sm:w-[400px] flex flex-col items-center xs:w-[300px]">
-            <img
-              className="w-[400px] h-[400px] rounded-md sm:w-[350px] sm:h-[350px] xs:w-[300px] xs:h-[300px]"
-              src={image.urls.regular}
-              alt={image.alt_description}
-            />
-            <h2 className="text-xl font-bold mt-[10px]">
-              {image.description ? image.description : image.alt_description}
-            </h2>
-            <p>
-              {image.likes} {image.likes === 1 ? "like" : "likes"}
-            </p>
-            <p>Updated by: {image.user.name}</p>
-            <p>
-              {image.user.portfolio_url && (
-                <div>
-                  User portfolio:{" "}
-                  <a
-                    target="_blank"
-                    rel="noreferrer"
-                    href={image.user.portfolio_url}
-                    className="font-semibold hover:underline"
-                  >
-                    {image.user.portfolio_url}
-                  </a>
-                </div>
-              )}
-            </p>
-            <div className="w-full mt-[20px] text-sm flex justify-evenly">
-              <button
-                onClick={() => {
-                  dispatch(downloadImage(image.id));
-                }}
-                className=" bg-primary-color text-white p-[10px] rounded-md hover:bg-primary-dark"
-              >
-                <Download sx={{ fontSize: 20 }} /> Download Image
-              </button>
-              <button
-                onClick={() => handleShare(image.id)}
-                className="border border-primary-color text-primary-color p-[8px] rounded-md hover:bg-primary-color hover:text-white"
-              >
-                <Share sx={{ fontSize: 20 }} /> Share Image
-              </button>
-            </div>
-          </div>
-        </div>
+        <SingleImage image={image} />
       )}
     </>
   );

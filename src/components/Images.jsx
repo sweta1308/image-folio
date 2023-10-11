35210cc2cc45e5b907d1a327bedfb6e3e43663bc
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 12:52:19 +0530

    feat: add list of images

---
+++ src/components/Images.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MoonLoader } from "react-spinners";
+import { fetchImages } from "../actions/imageAction";
+import { ImageCard } from "./ImageCard";
+
+export const Images = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchImages());
+  }, [dispatch]);
+  const state = useSelector((state) => state);
+  return (
+    <>
+      <div>
+        {state.images.length === 0 ? (
+          <div className="flex justify-center items-center mt-[60px]">
+            <MoonLoader color="#377dff" />
+          </div>
+        ) : (
+          <div className="flex flex-wrap justify-around">
+            {state.images.map((image) => (
+              <div key={image.id}>
+                <ImageCard image={image} />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};

003b1975d09b8ebd255204dea14c0e0ce7445633
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 19:50:39 +0530

    feat: add download functionality

--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,17 +1,20 @@
 import { Download, Share } from "@mui/icons-material";
 import { handleShare } from "../utils/handleShare";
 import { downloadImage } from "../actions/imageAction";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { handleDownload } from "../utils/handleDownload";
 
 export const CardModal = ({ imageId, setShowModal }) => {
   const dispatch = useDispatch();
+  const state = useSelector((state) => state.imageUrl);
   return (
     <>
       <div className="bg-white z-[20] absolute top-12 right-3 text-sm font-semibold rounded-sm">
         <p
           onClick={(e) => {
             e.stopPropagation();
-            dispatch(downloadImage(imageId));
+            dispatch(downloadImage(state));
+            state && handleDownload(state);
             setShowModal(false);
           }}
           className="p-[8px] hover:text-primary-color"

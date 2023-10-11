eaa1a35b732b0c2c55c297fd27ac30918a6a1e35
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-10-11 13:34:38 +0530

    add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1200px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "830px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      xs: { max: "470px" },
+    },
     extend: {
       colors: {
         "primary-color": "#377dff",
